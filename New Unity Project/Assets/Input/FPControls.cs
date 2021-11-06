093e55484cffd078ae15bac672f3004fcd6cddb1
aivxx <90653195+aivxx@users.noreply.github.com>
2021-11-06 10:54:42 -0400

    Edit FirstPerson Controls Input Actions
    Created Scripts for FPControls and FirstPerson Camera

---
+++ New Unity Project/Assets/Input/FPControls.cs
@@ -0,0 +1,176 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Input/FPControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @FPControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @FPControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""FPControls"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay Controls"",
+            ""id"": ""55ec1812-af57-4a19-b91d-042399419692"",
+            ""actions"": [
+                {
+                    ""name"": ""CameraLook"",
+                    ""type"": ""Value"",
+                    ""id"": ""15d06627-d571-4e63-bea5-149d0c8506a5"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""PlayerMove"",
+                    ""type"": ""Button"",
+                    ""id"": ""be103588-086f-4eaf-b71a-7cbd4fb3e240"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""019c03db-2963-4def-a07f-f258f263c0b2"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and Mouse"",
+                    ""action"": ""CameraLook"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard and Mouse"",
+            ""bindingGroup"": ""Keyboard and Mouse"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // Gameplay Controls
+        m_GameplayControls = asset.FindActionMap("Gameplay Controls", throwIfNotFound: true);
+        m_GameplayControls_CameraLook = m_GameplayControls.FindAction("CameraLook", throwIfNotFound: true);
+        m_GameplayControls_PlayerMove = m_GameplayControls.FindAction("PlayerMove", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay Controls
+    private readonly InputActionMap m_GameplayControls;
+    private IGameplayControlsActions m_GameplayControlsActionsCallbackInterface;
+    private readonly InputAction m_GameplayControls_CameraLook;
+    private readonly InputAction m_GameplayControls_PlayerMove;
+    public struct GameplayControlsActions
+    {
+        private @FPControls m_Wrapper;
+        public GameplayControlsActions(@FPControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @CameraLook => m_Wrapper.m_GameplayControls_CameraLook;
+        public InputAction @PlayerMove => m_Wrapper.m_GameplayControls_PlayerMove;
+        public InputActionMap Get() { return m_Wrapper.m_GameplayControls; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayControlsActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayControlsActions instance)
+        {
+            if (m_Wrapper.m_GameplayControlsActionsCallbackInterface != null)
+            {
+                @CameraLook.started -= m_Wrapper.m_GameplayControlsActionsCallbackInterface.OnCameraLook;
+                @CameraLook.performed -= m_Wrapper.m_GameplayControlsActionsCallbackInterface.OnCameraLook;
+                @CameraLook.canceled -= m_Wrapper.m_GameplayControlsActionsCallbackInterface.OnCameraLook;
+                @PlayerMove.started -= m_Wrapper.m_GameplayControlsActionsCallbackInterface.OnPlayerMove;
+                @PlayerMove.performed -= m_Wrapper.m_GameplayControlsActionsCallbackInterface.OnPlayerMove;
+                @PlayerMove.canceled -= m_Wrapper.m_GameplayControlsActionsCallbackInterface.OnPlayerMove;
+            }
+            m_Wrapper.m_GameplayControlsActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @CameraLook.started += instance.OnCameraLook;
+                @CameraLook.performed += instance.OnCameraLook;
+                @CameraLook.canceled += instance.OnCameraLook;
+                @PlayerMove.started += instance.OnPlayerMove;
+                @PlayerMove.performed += instance.OnPlayerMove;
+                @PlayerMove.canceled += instance.OnPlayerMove;
+            }
+        }
+    }
+    public GameplayControlsActions @GameplayControls => new GameplayControlsActions(this);
+    private int m_KeyboardandMouseSchemeIndex = -1;
+    public InputControlScheme KeyboardandMouseScheme
+    {
+        get
+        {
+            if (m_KeyboardandMouseSchemeIndex == -1) m_KeyboardandMouseSchemeIndex = asset.FindControlSchemeIndex("Keyboard and Mouse");
+            return asset.controlSchemes[m_KeyboardandMouseSchemeIndex];
+        }
+    }
+    public interface IGameplayControlsActions
+    {
+        void OnCameraLook(InputAction.CallbackContext context);
+        void OnPlayerMove(InputAction.CallbackContext context);
+    }
+}
