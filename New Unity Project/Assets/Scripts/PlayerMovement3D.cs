db18b1e52ef63563a62f0d90634f8dfa0409b2ac
aivxx <90653195+aivxx@users.noreply.github.com>
2021-11-06 11:31:07 -0400

    Create Player movement controls
    Also created scripts for smooth camera movement

---
+++ New Unity Project/Assets/Scripts/PlayerMovement3D.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement3D : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 20;
+    private Vector3 motion;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        motion = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        rb.velocity = motion * speed;
+    }
+}
